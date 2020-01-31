8367f197fe1f7bd9f9d6175854b96ecda061d767
Prakash T <prakash@convertcart.com>
2020-01-31 15:47:14 +0530

    Entire code refactor till iteration 2

--- assign_role.py
+++ assign_role.py
@@ -1,27 +1,17 @@
 import random;
 roles = ['killer','innocent']
-killerCount = 0
-innocentCount = 0
 
-def genRandomPosition(persons):
-    return random.choice(persons)
+print("Enter the number of players...")
+personsCount = input()
 
-def genRoles(result):    
-    while(1):
-        role = genRandomPosition(roles)
-        if role=="killer" and killerCount < 1:
-            killer += 1
-            return role
-        elif role=="innocent" and innocentCount <= personsCount-1:
-            innocentCount += 1
-            return role
-
-def genPersons(personsCount):
-    result = []; 
+def genPersons(killerPosition):
+    personsRole = []
     for i in range(personsCount):
-        role = genRoles(result)
-        result.append(role)
-    return result    
+        if(killerPosition == i):
+            personsRole.append("killer")
+        else:
+            personsRole.append("innocent")
+    return personsRole
 
 def genDeathDetails(persons,killerPosition):
     roundNumber = 0
@@ -32,30 +22,24 @@ def genDeathDetails(persons,killerPosition):
             roundNumber += 1
     return details
 
-def getKillerPosition(persons):
-    for position in range(len(persons)):
-        if persons[position] == 'killer':
-            return position
-
 def display(details):
     length = len(details)
     for index in range(length):
         print(details[index])
 
+
 def main():
     #iteration 1:
-    persons = genPersons(personsCount)
+    killerPosition = random.randint(0,personsCount-1)
+    persons = genPersons(killerPosition)
     display(persons)
-    
-    #iteration 2:
-    killerPosition = getKillerPosition(persons)
+
     print('\n\n')
+
+    # #iteration 2:
     deathDetails = genDeathDetails(persons,killerPosition)
     display(deathDetails)
 
-
-
-print("Enter the number of players...")
-personsCount = input()
+    #iteration 3:
 
 main()
