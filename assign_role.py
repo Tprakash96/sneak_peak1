a44f0c400097dc7a57852dea8c87babfbd872139
Prakash T <prakash@convertcart.com>
2020-01-31 17:46:34 +0530

    Added iteration3 module

--- assign_role.py
+++ assign_role.py
@@ -4,6 +4,9 @@ roles = ['killer','innocent']
 print("Enter the number of players...")
 personsCount = input()
 
+def genRandomNumber(start,end):
+    return random.randint(start,end)
+
 def genPersons(killerPosition):
     personsRole = []
     for i in range(personsCount):
@@ -22,15 +25,44 @@ def genDeathDetails(persons,killerPosition):
             roundNumber += 1
     return details
 
+def toKillOthers(persons,killerPosition):
+    roundCount = 1
+    while(1):
+        print("Round :"+str(roundCount))
+        isKillerSuspected = 0
+        suspectedCount = [0 for i in range(personsCount)]
+
+        for i in range(len(persons)):
+            randomPosition = genRandomNumber(0,len(persons)-1)
+            if randomPosition != killerPosition and i == killerPosition:
+                print("p"+str(i)+": kills "+"p"+str(randomPosition))
+            elif randomPosition != i:
+                suspectedCount[randomPosition] += 1
+                print("p"+str(i)+": suspects "+"p"+str(randomPosition))
+
+        for i in range(len(suspectedCount)):
+            print("p"+str(i)+": has been suspected "+str(suspectedCount[i]))
+
+        maxSuspected = suspectedCount.index(max(suspectedCount))
+
+        print("The maximum suspected count is "+str(maxSuspected))
+
+        if maxSuspected == killerPosition:
+            print "The Killer p"+str(killerPosition)+" has been suspected and killed in round "+str(roundCount)
+            break;
+        else:
+            print "The innocent p"+str(maxSuspected)+" has been left with killer in round "+str(roundCount)
+            break
+        roundCount += 1
+
 def display(details):
     length = len(details)
     for index in range(length):
         print(details[index])
 
-
 def main():
     #iteration 1:
-    killerPosition = random.randint(0,personsCount-1)
+    killerPosition = genRandomNumber(0,personsCount-1)
     persons = genPersons(killerPosition)
     display(persons)
 
@@ -40,6 +72,8 @@ def main():
     deathDetails = genDeathDetails(persons,killerPosition)
     display(deathDetails)
 
-    #iteration 3:
+    print('\n\n')
 
+    #iteration 3:
+    toKillOthers(persons,killerPosition)
 main()
